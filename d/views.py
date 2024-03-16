89aea36c0e7894f15ef5e2033ee43025ac0e8455
G'ayrat Sayfullayev <136235060+sayfullayev0204@users.noreply.github.com>
2024-03-16 14:03:50 +0500

    Update views.py

--- d/views.py
+++ d/views.py
@@ -17,3 +17,33 @@ def guruhlash(request):
         return render(request, 'guruh.html', {'result': result})
     else:
         return render(request, 'guruh.html')
+def takroriy(request):
+    if request.method == 'POST':
+        n = int(request.POST.get('entry1'))
+        m = int(request.POST.get('entry2'))
+        if m<n:
+            result = "m soni n dan katta bulishi kerak qayta urinib koring"
+        else:
+            c=math.factorial(m+n-1)/(math.factorial(n)*math.factorial(m-1))
+            result = f"={c}"
+        return render(request, 'takroriy.html', {'result': result})
+    else:
+        return render(request, 'takroriy.html')
+        
+def orin(request):
+    if request.method == 'POST':
+        n = int(request.POST.get('entry1'))
+        m = int(request.POST.get('entry2'))
+        c = math.factorial(m)/math.factorial(n)
+        result = f"={c}"
+        return render(request, 'orin.html', {'result':result})
+    else:
+        return render(request, 'orin.html')
+def p(request):
+    if request.method == 'POST':
+        n = int(request.POST.get('entry1'))
+        c = math.factorial(n)
+        result = f"={c}"
+        return render(request, 'p.html', {'result':result})
+    else:
+        return render(request, 'p.html')
