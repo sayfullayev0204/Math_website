384c86188562d7979e2ebe328e25bc5cdb1902a1
G'ayrat Sayfullayev <136235060+sayfullayev0204@users.noreply.github.com>
2024-03-16 14:03:21 +0500

    Update urls.py

--- d/urls.py
+++ d/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import home,guruhlash
+from .views import home,guruhlash,takroriy, orin,p
 urlpatterns=[
     path('', home, name='home' ),
-    path('guruh/', guruhlash, name="guruh")
+    path('guruh/', guruhlash, name="guruh"),
+    path('takroriy/', takroriy, name='takroriy'),
+    path('orin/', orin, name='orin'),
+    path('url/', p, name='url')
 ]
